ac70c062d18d1be890d2d6f0fd65414fb6af320c
Arni B <arnibarnason@gmail.com>
2015-05-13 10:01:27 +0000

    comments added

--- maskul.py
+++ maskul.py
@@ -11,7 +11,7 @@ class MyParser(argparse.ArgumentParser):
         self.print_help()
         sys.exit(2)
 
-
+# Set the arguments
 parser = argparse.ArgumentParser(description="""Returns a project on Myschool. First parameter (optional) preceding a '-c' contains a comment to the teacher and the second parameter (optional) preceding a '-f' contains the path (as a string) of the file to turn in""")
 parser.add_argument('-c', nargs='?', action='store', dest='c', help='Comment to the teacher')
 parser.add_argument('-f', nargs='?', action='store', dest='f', help='Path to the file to turn in')
@@ -19,16 +19,19 @@ parser.add_argument('p', help='Path to the projects website')
 args = parser.parse_args()
 url = args.p
 
+# Prompt user for Username and Password
 authentication = (input('Username: '), getpass())
 res = r.get(url, auth=authentication)
 
+# Store the arguments
 filePath = args.f
 comment = args.c
 
+# Find the url of the handin server
 soup = BeautifulSoup(res.text)
-
 formURL = soup.find('form', id='form1')['action']
 
+# Add file to upload
 if not filePath is None:
     fileToUpload = {'file': open(filePath, 'rb')}
 else:
@@ -36,5 +39,6 @@ else:
 
 data = {'athugasemdnemanda': comment}
 
+# Post the data and file to myschool
 rex = r.post('https://myschool.ru.is/myschool/'+formURL, 
     data=data, files=fileToUpload, auth=authentication)
